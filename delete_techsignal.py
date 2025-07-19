9bb85baf98a41b7fe5258664c7272a8c3126f91d
Сергей Б <sbalymov@ptsecurity.com>
2025-07-19 17:13:05 +0300

    delete techsignal

---
+++ delete_techsignal.py
@@ -0,0 +1,65 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+cookies = ""
+
+def auth():
+    global cookies
+    url = f"https://{isim_ip}/httpapi/login"
+    headers = {"Content-Type": "application/json"}
+    
+    payload = {
+        "login": isim_login,
+        "password": isim_pass,
+        "domain":""
+    }
+    
+    response_auth = requests.post(url, json=payload, headers=headers, verify=False)
+    if response_auth.status_code == 200:
+        print("auth ok")
+        cookies = response_auth.cookies
+    else:
+        print("auth fail")
+        exit()
+
+
+def delete_tech_sig(id):
+    url_del_tech_id = f"https://{isim_ip}/api/techsignal/{id}"
+    headers = {"Content-Type": "application/json, text/plain, */*"}
+    response = requests.delete(url_del_tech_id, headers=headers, verify=False, cookies=cookies) 
+    return response.status_code
+    
+def get_tech_table(): 
+    auth()
+    url_tech_tables  = f"https://{isim_ip}/api/techsignal/table"
+    payload = {"page":{"limit":1000,"offset":0,"sortFields":[{"field":"createTime","direction":1}]},"filters":{}}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url_tech_tables, json=payload, headers=headers, verify=False, cookies=cookies)   
+    
+    data = response.json()
+
+    for item in data["data"]:
+        # ищем где в имене есть key_word   
+        if key_word in item["name"]:
+                 
+            if delete:
+                status_code = delete_tech_sig(item.get('id',''))
+                if status_code == 200:
+                    print(f"DELETE: {item.get('id','')}:  {item.get('name','')}")
+                else:
+                    print(f"Error: {status_code}   {item.get('id','')}:  {item.get('name','')}")
+          
+            else:
+                print(f"{item.get('id','')}:  {item.get('name','')}")
+
+
+isim_ip = "10.50.94.12"
+isim_login =  "login"
+isim_pass = "password"
+key_word = "area"
+delete = 0
+
+
+
+get_tech_table()
